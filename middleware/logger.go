c272302f41c016d0af5a2ec071986edf7258e9ab
Cr7@tndstd.com <github-cr7123>
2021-08-02 16:43:46 +0800

    add 请求包，响应包

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,13 +1,24 @@
 package middleware
 
 import (
-	"time"
-
+	"bytes"
+	"io/ioutil"
 	"self-discipline/global"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
+
+type bodyLogWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w bodyLogWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
 
 // GinLogger 接收gin框架默认的日志
 func GinLogger() gin.HandlerFunc {
@@ -15,6 +26,20 @@ func GinLogger() gin.HandlerFunc {
 		start := time.Now()
 		path := c.Request.URL.Path
 		query := c.Request.URL.RawQuery
+
+		// Read the Body content
+		var bodyBytes []byte
+		if c.Request.Body != nil {
+			bodyBytes, _ = ioutil.ReadAll(c.Request.Body)
+		}
+		// Restore the io.ReadCloser to its original state
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+		blw := &bodyLogWriter{
+			body:           bytes.NewBufferString(""),
+			ResponseWriter: c.Writer,
+		}
+		c.Writer = blw
 		c.Next()
 
 		cost := time.Since(start)
@@ -23,9 +48,11 @@ func GinLogger() gin.HandlerFunc {
 			zap.String("method", c.Request.Method),
 			zap.String("path", path),
 			zap.String("query", query),
+			zap.String("queryBody", string(bodyBytes)),
 			zap.String("ip", c.ClientIP()),
 			zap.String("user-agent", c.Request.UserAgent()),
 			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.String("responseBody", string(blw.body.Bytes())),
 			zap.Duration("cost", cost),
 		)
 	}
