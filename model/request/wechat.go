3a786e54af00ded416f6daa2f4862a14538d5195
hsuan-jen <719794907@qq.com>
2021-08-05 23:30:19 +0800

    edit

--- model/request/wechat.go
+++ model/request/wechat.go
@@ -2,7 +2,7 @@ package request
 
 type CheckSignature struct {
 	Signature string `json:"signature" form:"signature"`
-	Timestamp int16  `json:"timestamp" form:"timestamp"`
+	Timestamp int    `json:"timestamp" form:"timestamp"`
 	Nonce     string `json:"nonce" form:"nonce"`
 	Echostr   string `json:"echostr" form:"echostr"`
 }
