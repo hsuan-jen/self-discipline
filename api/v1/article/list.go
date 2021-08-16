eb0488700c19edac8c6de0c91b8855705cd70a1d
Cr7@tndstd.com <github-cr7123>
2021-08-16 18:03:40 +0800

    add 数据验证

--- api/v1/article/list.go
+++ api/v1/article/list.go
@@ -1,19 +1,29 @@
 package article
 
 import (
+	"self-discipline/global"
 	articleReq "self-discipline/model/article/request"
 	"self-discipline/model/common/response"
+	"self-discipline/utils"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func (h *Handler) GetList(ctx *gin.Context) {
 	var req articleReq.GetList
 	_ = ctx.ShouldBind(&req)
 
+	if err := utils.Verify(req.PageInfo, utils.PageInfoVerify); err != nil {
+		response.FailWithMessage(err.Error(), ctx)
+		return
+	}
+
 	list, err := articleService.GetList(&req)
 	if err != nil {
-
+		global.LOG.Error("查询动态列表失败！", zap.Any("err", err))
+		response.FailWithMessage(err.Error(), ctx)
+		return
 	}
 
 	response.OkWithData(list, ctx)
--- utils/verify.go
+++ utils/verify.go
@@ -5,4 +5,5 @@ var (
 	RegisterVerify    = Rules{"Phone": {NotEmpty()}, "Password": {NotEmpty()}}
 	WechatLoginVerify = Rules{"Token": {NotEmpty()}}
 	IssueVerify       = Rules{"Content": {NotEmpty()}}
+	PageInfoVerify    = Rules{"Page": {NotEmpty()}, "PageSize": {NotEmpty(), Le("15"), Ge("1")}}
 )
